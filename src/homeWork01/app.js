695764fd9dbe3ba86bf7cfd11c3c7a755890d4ba
Revsky Andrey <andrey.g1362@gmail.com>
2020-11-03 11:44:30 +0200

    finish task

--- src/homeWork01/app.js
+++ src/homeWork01/app.js
@@ -1,11 +1,11 @@
-const task = require('./task');
+const { task1: firstTask, task2: secondTask, task3 } = require('./task');
 
 const goods = require('./goods.json');
 
 boot(goods, 'type', 'socks');
 
 function boot(goods, type, value) {
-  console.log('task1 = ', task.task1(goods, type, value));
-  console.log('task3 = ', task.task3(goods));
-  console.log('task2 = ', task.task2);
+  console.log('task1 = ', firstTask(goods, type, value));
+  console.log('task3 = ', task3(goods));
+  console.log('task2 = ', secondTask);
 }
