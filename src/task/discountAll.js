fb2ed5c1335b0a5e1aaf19a44e178af9f1da0505
RevskyAndrey <andrey.g1362@gmail.com>
2020-11-15 21:57:21 +0200

    finish function getDiscountAllItems

--- src/task/discountAll.js
+++ src/task/discountAll.js
@@ -2,28 +2,41 @@ const myMap = require('./myMap');
 const getDiscount = require('./discount');
 const goods = require('../goods.json');
 
-function priceСalculation(price, discount) {
+function priceCalculation(price, discount) {
   return (price.slice(1) * (1 - discount / 100)).toFixed(2);
+}
+
+function checkDiscount(cost) {
+  const discount = getDiscount();
+
+  if (discount === 'not your day') return cost;
+  return priceCalculation(cost, discount);
 }
 
 function getDiscountAllItems() {
   myMap(goods, (item) => {
-    item.discount = getDiscount();
-    let cost = item.price || item.priceForPair;
-    if (item.discount === 'not your day') {
-      item.discount = '0%';
-      item.newPrice = cost;
-    } else {
-      item.newPrice = priceСalculation(cost, item.discount);
-      item.discount += '%';
+    const cost = item.price || item.priceForPair;
+    if (!item.quantity) item.quantity = 0;
+
+    if (item.type === 'hat') {
+      item.newPrice = checkDiscount(cost);
+      item.newPrice = checkDiscount(item.newPrice);
     }
 
+    if (item.type === 'hat' && item.color === 'red') {
+      item.newPrice = checkDiscount(cost);
+      item.newPrice = checkDiscount(item.newPrice);
+      item.newPrice = checkDiscount(item.newPrice);
+    }
+
+    item.newPrice = checkDiscount(cost);
+    item.newPrice += '$';
     return item;
   });
 
   return goods;
 }
 
-module.exports = (data) => {
+module.exports = () => {
   return getDiscountAllItems();
 };
