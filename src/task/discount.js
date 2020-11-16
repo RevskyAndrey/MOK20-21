24a9c82edee745ebddcbed3aa8f055c619d3126a
RevskyAndrey <andrey.g1362@gmail.com>
2020-11-16 21:13:50 +0200

    savepoint

--- src/task/discount.js
+++ src/task/discount.js
@@ -1,14 +1,12 @@
 function getRandomInt() {
-  // eslint-disable-next-line no-param-reassign
   const min = Math.ceil(1);
-  // eslint-disable-next-line no-param-reassign
   const max = Math.floor(99);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getDiscount(data) {
   const maxDiscount = 20;
-  const yourDiscont = getRandomInt(1, 99);
+  const yourDiscont = getRandomInt();
   // eslint-disable-next-line no-param-reassign
   if (yourDiscont <= maxDiscount) data = yourDiscont;
   else {
