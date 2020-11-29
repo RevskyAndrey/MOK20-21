9bb440aca1735db3cc290c436675d0b72b0ec0e7
RevskyAndrey <andrey.g1362@gmail.com>
2020-11-29 21:58:01 +0200

    fix some err,  make task 2

--- src/server/controllers/controller.js
+++ src/server/controllers/controller.js
@@ -1,13 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const { nanoid } = require('nanoid');
-
-const { pipeline } = require('stream');
-const { createGunzip } = require('zlib');
-const { promisify } = require('util');
 
 const pathToFile = path.resolve(__dirname, '../', 'goods.json');
-const cvsToJSON = require('../utils/CSV-to-JSON');
+
 const goods = require('../../goods.json');
 const {
   task1: firstTask,
@@ -17,7 +12,6 @@ const {
   myMap,
 } = require('../task');
 
-const promisifiedPipeline = promisify(pipeline);
 let resultArr = [];
 
 function home(response) {
@@ -70,35 +64,11 @@ async function discountAll(response) {
   }
 }
 
-// POST
-// 127.0.0.1:3000/changeJSON
+// POST  127.0.0.1:3000/changeJSON
 function changeJSON(data, response) {
   fs.writeFileSync(pathToFile, JSON.stringify(data, null, 1));
   response.write('Post result = ');
   response.end(JSON.stringify(data));
-}
-
-// POST
-// 127.0.0.1:3000/loadingCSV
-
-async function uploadCSV(inputStream, response) {
-  const uploadDir = process.env.UPLOAD_DIR;
-  const gunzip = createGunzip();
-  const filePath = `${uploadDir}${nanoid(8)}-${Date.now()}-${nanoid(4)}.json`;
-  const outputStream = fs.createWriteStream(filePath);
-  try {
-    await promisifiedPipeline(inputStream, gunzip, cvsToJSON(), outputStream);
-  } catch (err) {
-    console.err('err', err);
-  }
-
-  try {
-    await fs.promises.rm(filePath);
-  } catch (rmErr) {
-    console.error(`Unable to remove JSON ${filePath}`, rmErr);
-    throw new Error('Unable to remove JSON');
-  }
-  response.end(filePath);
 }
 
 module.exports = {
@@ -108,5 +78,4 @@ module.exports = {
   task3,
   discountAll,
   changeJSON,
-  uploadCSV,
 };
