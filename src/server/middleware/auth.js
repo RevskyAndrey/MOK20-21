d7f99c40364318a5302dc008ea9b9c74db303675
andrew <andrey.g1362@gmail.com>
2021-01-20 16:22:40 +0200

    fix error

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -3,13 +3,16 @@ const { accessTokenSecret } = require('../../config');
 
 
 function authentificate(req, res, next) {
-  const token = req.headers.authorization.split(' ')[1];
-  if (!token) res.send('not authorization').status(403);
-  jwt.verify(token, accessTokenSecret, (err, user) => {
-    if (err) throw new Error('Token expired!');
-    req.user = user;
-    next();
-  });
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) res.send('not authorization').status(403);
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) throw new Error('Token expired!');
+      req.user = user;
+      next();
+    });
+  }
+  res.send('not authorization').status(403);
 }
 
 module.exports = authentificate;
